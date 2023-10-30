921d9084a8c5389089526c8f8e3941c894d400a7
alexmorales12714 <alexis.morales@my.metrostate.edu>
2023-10-30 17:20:06 -0500

    event modification

--- .main/aalambanafoundation_repository/php/aalambana/delete_event.php
+++ .main/aalambanafoundation_repository/php/aalambana/delete_event.php
@@ -1,14 +1,35 @@
 <?php
-include 'dbconnect.php';
+require 'db_configuration.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id'])) {
-    $id = $_POST['id'];
-
-    $stmt = $conn->prepare("DELETE FROM events WHERE Event_Id = ?");
-    $stmt->execute([$id]);
-
-    echo "Event deleted successfully!";
+$status = session_status();
+if ($status == PHP_SESSION_NONE) {
+    session_start();
 }
 
-// Here, you can add a list of events with a delete button next to each one or redirect to the main events page.
+$eventId = $_GET['event_id']; // Get the Event_Id from the URL parameter
+
+$connection = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+// Prepare SQL queries to delete data based on event_id
+$deleteEventQuery = "DELETE FROM events WHERE event_id='$eventId'";
+$deletePicturesQuery = "DELETE FROM event_pictures WHERE event_id='$eventId'";
+
+
+// Execute the delete queries
+if (mysqli_query($connection, $deleteEventQuery) &&
+    mysqli_query($connection, $deletePicturesQuery)) {
+    
+    echo "Event deleted successfully.";
+} else {
+    // Error occurred while deleting the event
+    echo "Error: " . mysqli_error($connection);
+}
+
+mysqli_close($connection);
+
+header('Location: events.php');
 ?>
