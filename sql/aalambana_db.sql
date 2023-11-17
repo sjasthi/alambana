2d37a7242a1b848738d16192df8df0ecf86eb266
Israel <israel.love@my.metrostate.edu>
2023-11-16 21:25:21 -0600

    Update aalambana_db.sql

--- sql/aalambana_db.sql
+++ sql/aalambana_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2023 at 03:37 AM
+-- Generation Time: Nov 17, 2023 at 04:23 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -34,21 +34,25 @@ CREATE TABLE `blogs` (
   `Description` text NOT NULL,
   `Video_Link` varchar(200) DEFAULT NULL,
   `Modified_Time` datetime DEFAULT NULL,
-  `Created_Time` datetime DEFAULT NULL
+  `Created_Time` datetime DEFAULT NULL,
+  `hash` varchar(200) NOT NULL,
+  `hidden` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `blogs`
 --
 
-INSERT INTO `blogs` (`Blog_Id`, `Title`, `Author`, `Description`, `Video_Link`, `Modified_Time`, `Created_Time`) VALUES
-(39, 'Blog Title1', 'Author1', 'Description1', 'https://youtu.be/k9em7Ey00xQ', '2023-11-06 20:33:09', '2023-11-06 20:33:09'),
-(40, 'Blog Title2', 'Author2', 'Description2', '', '2023-11-06 20:33:37', '2023-11-06 20:33:37'),
-(41, 'Blog Title3', 'Author3', 'Description3', '', '2023-11-06 20:34:08', '2023-11-06 20:34:08'),
-(42, 'Blog Title4', 'Author4', 'Description4', '', '2023-11-06 20:34:37', '2023-11-06 20:34:37'),
-(43, 'Blog Title5', 'Author5', 'Description5', '', '2023-11-06 20:35:24', '2023-11-06 20:35:24'),
-(44, 'Blog Title6', 'Author6', 'Description6', '', '2023-11-06 20:35:46', '2023-11-06 20:35:46'),
-(45, 'Blog Title7', 'Author7', 'Description7', '', '2023-11-06 20:36:11', '2023-11-06 20:36:11');
+INSERT INTO `blogs` (`Blog_Id`, `Title`, `Author`, `Description`, `Video_Link`, `Modified_Time`, `Created_Time`, `hash`, `hidden`) VALUES
+(39, 'Blog Title1', 'Author1', 'Description1', 'https://youtu.be/k9em7Ey00xQ', '2023-11-06 20:33:09', '2023-11-06 20:33:09', '', 0),
+(40, 'Blog Title2', 'Author2', 'Description2', '', '2023-11-06 20:33:37', '2023-11-06 20:33:37', '', 0),
+(41, 'Blog Title3', 'Author3', 'Description3', '', '2023-11-06 20:34:08', '2023-11-06 20:34:08', '', 0),
+(42, 'Blog Title4', 'Author4', 'Description4', '', '2023-11-06 20:34:37', '2023-11-06 20:34:37', '', 1),
+(43, 'Blog Title5', 'Author5', 'Description5', '', '2023-11-06 20:35:24', '2023-11-06 20:35:24', '', 0),
+(44, 'Blog Title6', 'Author6', 'Description6', '', '2023-11-06 20:35:46', '2023-11-06 20:35:46', '', 0),
+(45, 'Blog Title7', 'Author7', 'Description7', '', '2023-11-06 20:36:11', '2023-11-06 20:36:11', '', 0),
+(76, 'A2', 'Love, Israel', 'A2', '', '2023-11-17 00:07:40', '2023-11-17 00:07:40', '$2y$10$RE2YqufUEbyc66NQLqD9XOVycSKn4PDr2oxNTV5qj2svUHGk9z326', 0),
+(77, 'A3', 'Love, Israel', 'A3', '', '2023-11-17 00:07:45', '2023-11-17 00:07:45', '$2y$10$RE2YqufUEbyc66NQLqD9XOVycSKn4PDr2oxNTV5qj2svUHGk9z326', 1);
 
 -- --------------------------------------------------------
 
@@ -61,8 +65,33 @@ CREATE TABLE `blog_comments` (
   `Blog_Id` int(11) DEFAULT NULL,
   `Paragraph` text NOT NULL,
   `Created_Time` timestamp NULL DEFAULT NULL,
-  `Name` varchar(50) DEFAULT NULL
+  `Name` varchar(50) DEFAULT NULL,
+  `Subject_Id` int(11) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `blog_comments`
+--
+
+INSERT INTO `blog_comments` (`Comment_Id`, `Blog_Id`, `Paragraph`, `Created_Time`, `Name`, `Subject_Id`, `email`, `url`) VALUES
+(163, 43, 'test1', '2023-11-15 10:49:14', 'user name 1', 1, 'istreamtofly@gmail.com', ''),
+(164, 43, 'sddfsdf', '2023-11-15 11:00:26', 'user name 2', 1, 'istreamtofly@gmail.com', ''),
+(165, 43, 'sdfsdfa44', '2023-11-15 11:00:51', 'user name 2', 2, 'istreamtofly@gmail.com', ''),
+(166, 43, 'testsesete3', '2023-11-15 11:01:09', 'Anonymous User', 3, 'istreamtofly@gmail.com', ''),
+(167, 43, 'testees34555', '2023-11-15 11:01:53', 'Israel', 1, 'israel.love@my.metrostate.edu', ''),
+(168, 43, 'ddd', '2023-11-15 11:02:02', 'Israel', 1, 'israel.love@my.metrostate.edu', ''),
+(169, 43, 'eeeeeeeeeeeeeeeeee', '2023-11-15 11:02:11', 'Israel', 3, 'israel.love@my.metrostate.edu', ''),
+(170, 43, 'AAAAAAAAAAAAAAAAAAA', '2023-11-15 11:10:31', 'Administration', 1, 'cs320@silcmn.com', ''),
+(171, 43, 'sdffAAAAAAAAAAAAAA', '2023-11-15 11:10:40', 'Administration', 4, 'cs320@silcmn.com', ''),
+(172, 43, 'Aaaaaaaaaa', '2023-11-15 11:10:50', 'Administration', 4, 'cs320@silcmn.com', ''),
+(173, 45, 'admin post1', '2023-11-15 22:54:00', 'Administration', 1, 'cs320@silcmn.com', ''),
+(174, 77, 'test1', '2023-11-17 09:52:48', 'user name 1', 1, 'istreamtofly@gmail.com', ''),
+(175, 77, 'test 2', '2023-11-17 09:53:26', 'Anonymous User', 2, 'istreamtofly@gmail.com', ''),
+(176, 45, 'comment 2', '2023-11-17 10:10:45', 'Administration', 1, 'cs320@silcmn.com', ''),
+(177, 45, 'Comment 3 new', '2023-11-17 10:11:01', 'Administration', 2, 'cs320@silcmn.com', ''),
+(178, 45, 'Hello to Comment 2', '2023-11-17 10:11:41', 'Israel', 1, 'israel.love@my.metrostate.edu', '');
 
 -- --------------------------------------------------------
 
@@ -87,7 +116,12 @@ INSERT INTO `blog_pictures` (`Picture_Id`, `Blog_Id`, `Location`) VALUES
 (24, 42, 'images/blog_pictures/65493fcd93bd00.75092656.jpg'),
 (25, 43, 'images/blog_pictures/65493ffc9b0a39.52529038.jpg'),
 (26, 44, 'images/blog_pictures/654940129a4f76.32594610.jpg'),
-(27, 45, 'images/blog_pictures/6549402b13df46.83020739.jpg');
+(27, 45, 'images/blog_pictures/6549402b13df46.83020739.jpg'),
+(28, 48, 'images/blog_pictures/6554f41890b446.42606833.jpg'),
+(29, 49, 'images/blog_pictures/6554f621909a87.79369267.jpg'),
+(30, 50, 'images/blog_pictures/6554f6a96b8172.66056853.jpg'),
+(56, 76, 'images/blog_pictures/6556a0bc349ff7.96590254.jpg'),
+(57, 77, 'images/blog_pictures/6556a0c190e638.79451381.jpg');
 
 -- --------------------------------------------------------
 
@@ -109,7 +143,8 @@ CREATE TABLE `blog_story` (
 INSERT INTO `blog_story` (`Story_Id`, `Blog_Id`, `Paragraph`, `About_Author`) VALUES
 (22, 39, 'Blog Paragraph1', 'Author Description1'),
 (23, 45, 'Blog Paragraph7', 'Author Description7'),
-(24, 44, 'Blog Paragraph6', 'Author Description6');
+(24, 44, 'Blog Paragraph6', 'Author Description6'),
+(25, 51, 'Blog Paragraph', 'Author Description');
 
 -- --------------------------------------------------------
 
@@ -119,13 +154,21 @@ INSERT INTO `blog_story` (`Story_Id`, `Blog_Id`, `Paragraph`, `About_Author`) VA
 
 CREATE TABLE `events` (
   `Event_Id` int(11) NOT NULL,
-  `Title` varchar(100) DEFAULT NULL,
+  `Title` int(100) DEFAULT NULL,
   `Description` text DEFAULT NULL,
   `Video_Link` varchar(255) DEFAULT NULL,
   `Event_Date` datetime DEFAULT NULL,
   `Created_Time` datetime DEFAULT NULL,
   `Modified_Time` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`Event_Id`, `Title`, `Description`, `Video_Link`, `Event_Date`, `Created_Time`, `Modified_Time`) VALUES
+(2, 0, ' Add new Event Event Title Event Title Event Description', '', '2023-11-15 12:28:00', '2023-11-10 19:28:20', '2023-11-10 19:28:20'),
+(3, 0, 'Event Description2', '', '2023-11-15 19:43:00', '2023-11-14 02:43:20', '2023-11-14 02:43:20');
 
 -- --------------------------------------------------------
 
@@ -144,7 +187,8 @@ CREATE TABLE `event_pictures` (
 
 INSERT INTO `event_pictures` (`Event_Id`, `Location`) VALUES
 (1, 'images/event_pictures/654abb793cf0d.'),
-(2, 'images/event_pictures/654abb9ccf77a.jpg');
+(2, 'images/event_pictures/654abb9ccf77a.jpg'),
+(3, 'images/event_pictures/6552d0b8d31ea.');
 
 -- --------------------------------------------------------
 
@@ -272,37 +316,37 @@ ALTER TABLE `user_photos`
 -- AUTO_INCREMENT for table `blogs`
 --
 ALTER TABLE `blogs`
-  MODIFY `Blog_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `Blog_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
 
 --
 -- AUTO_INCREMENT for table `blog_comments`
 --
 ALTER TABLE `blog_comments`
-  MODIFY `Comment_Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Comment_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=179;
 
 --
 -- AUTO_INCREMENT for table `blog_pictures`
 --
 ALTER TABLE `blog_pictures`
-  MODIFY `Picture_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `Picture_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `blog_story`
 --
 ALTER TABLE `blog_story`
-  MODIFY `Story_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `Story_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `Event_Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Event_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `event_pictures`
 --
 ALTER TABLE `event_pictures`
-  MODIFY `Event_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Event_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
