455e91180fa4281bc3d1cafc33a238eb0692f560
Michael Scarr <michael.m.scarr@gmail.com>
2024-02-26 12:43:17 -0600

    added new_db.sql, my proposal for the new database

---
+++ sql/new_db.sql
@@ -0,0 +1,74 @@
+CREATE SCHEMA `aalambana_db2`;
+
+USE aalambana_db2;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    about TEXT,
+    email VARCHAR(255),
+    hash VARCHAR(255),
+    active VARCHAR(10),
+    role ENUM('Administrator', 'User', 'Moderator') DEFAULT 'User',
+    modified_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('enabled', 'disabled') DEFAULT 'enabled'
+);
+
+CREATE TABLE events (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    information TEXT,
+    video_link VARCHAR(2048),
+    event_date_start DATE NOT NULL,
+    event_date_end DATE NOT NULL,
+    modified_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    attendees INT DEFAULT 0,
+    location VARCHAR(255),
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE blogs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    content TEXT,
+    video_link VARCHAR(2048),
+    modified_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL,
+    hidden TINYINT DEFAULT 0,
+    visitor_count INT DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT,
+    modified_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    blog_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (blog_id) REFERENCES blogs(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE pictures (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    location VARCHAR(2048),
+    blog_id INT,
+    user_id INT NOT NULL,
+    event_id INT,
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (blog_id) REFERENCES blogs(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (event_id) REFERENCES events(id)
+);
+
+ALTER TABLE users
+ADD COLUMN picture_id INT,
+ADD FOREIGN KEY (picture_id) REFERENCES pictures(id);
