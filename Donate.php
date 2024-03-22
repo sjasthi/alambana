9bcff5151e633ab65fefb6622e1ff13ecf7a4060
Abdirahman-Ainab <abdirahman.ainab@my.metrostate.edu>
2024-03-21 23:33:37 -0500

    Update Donate.php

--- Donate.php
+++ Donate.php
@@ -18,7 +18,7 @@
 <!-- Basic Page Needs
   ================================================== -->
 <link rel="icon" href="favicon.ico" type="image/x-icon">
-<title>Typography</title>
+<title>Donate</title>
 <meta name="description" content="">
 <meta name="keywords" content="">
 <meta name="author" content="">
