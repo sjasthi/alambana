6320f03ea1ca176c292dc7239885e5e3aa0be36c
Abdirahman-Ainab <abdirahman.ainab@my.metrostate.edu>
2024-04-08 16:11:33 -0500

    Update donate.php

--- donate.php
+++ donate.php
@@ -8,6 +8,7 @@ $stripe_secret_key = "sk_test_51P0aNgRqID97Nih52PUAEMxPM2kOO3Gma8AaWKHkSCTf1AEbv
 
 $checkout_session = \Stripe\Checkout\Session::create([
     "mode" => "payment",
+    'submit_type' => 'donate',
     "success_url" => "http://localhost/alambana/success.php",
     "cancel_url" => "http://localhost/alambana",
     "locale" => "auto",
