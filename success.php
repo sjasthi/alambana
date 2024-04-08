ca7728f70e9b75d05cf4b0e09de8b1b4d84d2e3c
Abdirahman-Ainab <abdirahman.ainab@my.metrostate.edu>
2024-04-07 22:19:44 -0500

    Stripe donation page redirection. Test donation page
    Make sure to enter ( composer require stripe/stripe-php ) into the project root directory in power shell to download Stripe library

---
+++ success.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Thanks for your order!</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <section id="success" class="hidden">
+    <p>
+      We appreciate your business! A confirmation email will be sent to <span id="customer-email"></span>.
+
+      If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
+    </p>
+  </section>
+
+  <form method="post" action="index.php">
+        <p>Return to Aalambana Foundation Home Page</p>
+        <button>Return</button>
+    </form>
+</body>
+</html>
