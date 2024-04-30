036735c548ab1e4f5d447c65ac09ec0e1fc0430e
Abdirahman-Ainab <abdirahman.ainab@my.metrostate.edu>
2024-04-30 15:26:23 -0500

    Added more instructions for Stripe setup

--- donate.php
+++ donate.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
+# Make sure to create a Stripe account for test mode and activate the account for real payments.
+# Enter Stripe secret key below (Located at the Developer and api key section on the Stripe website). 
 $stripe_secret_key = "sk_test_51P0aNgRqID97Nih52PUAEMxPM2kOO3Gma8AaWKHkSCTf1AEbvgbpR8q8zh2rvhjO2Joo1P3toSYsGeB2b9oBL1Ha00OKMc1Uhg";
 
 \Stripe\Stripe::setApiKey($stripe_secret_key);
@@ -9,11 +11,11 @@ $stripe_secret_key = "sk_test_51P0aNgRqID97Nih52PUAEMxPM2kOO3Gma8AaWKHkSCTf1AEbv
 $checkout_session = \Stripe\Checkout\Session::create([
     "mode" => "payment",
     'submit_type' => 'donate',
-    "success_url" => "https://aalambana.jasthi.com/success.php",
-    "cancel_url" => "https://aalambana.jasthi.com/",
+    "success_url" => "https://aalambana.jasthi.com/success.php", # update this section with the right domain
+    "cancel_url" => "https://aalambana.jasthi.com/",    # update this section with the right domain
     "locale" => "auto",
     'line_items' => [[
-        # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        # Provide the exact Price ID (e.g. pr_1234) of the product/page you want to showcase (Found under product catalog)
         'price' => 'price_1P0bW5RqID97Nih5CUbqqPwy',
         'quantity' => 1,
       ]],
